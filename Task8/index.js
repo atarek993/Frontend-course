543acddef5012b3cb22374ab65f4591261714b68
Ahmed Tarek <105000643+atarek993@users.noreply.github.com>
2024-09-27 14:44:09 +0300

    javascript 1st task

---
+++ Task8/index.js
@@ -0,0 +1,86 @@
+//1-
+// var num1 = prompt("please enter a number")
+// if (num1 < 100)
+// {
+//     console.log("number is less than 100 ")
+// }
+// else if (num1 == 100)
+//     {
+//     console.log("number is equal to 100")
+
+// }
+// else
+// {
+//     console.log("number is greater than 100")
+// }
+
+
+//2-
+
+
+
+
+//3-
+
+// var password = prompt("Please enter a password ")
+// var confirm_password = "123ahmed123"
+
+// if (password==confirm_password)
+// {
+//     console.log("password is valid")
+// }
+
+// else{
+//     console.log("password isn't valid")
+
+// }
+
+//4-
+// var day = prompt("please enter a day")
+// if (day == "saturday"|| day== "sunday" || day == "Saturday" || day== "Sunday")
+// {
+//     console.log("it's weekend")
+// }
+
+// else{
+//     console.log("it's workday")
+
+// }
+
+//5-
+
+// var num1 = prompt("Please enter a number")
+
+// if (num1 < 0)
+// {
+//     console.log("Number is negative")
+// }
+// else
+// {
+//     console.log("Number is Positive")
+
+// }
+
+//6-
+// var academic_grade = prompt("Please enter your academic garde")
+
+// if (academic_grade < 50)
+// {
+//     console.log("Your grade is F")
+// }
+// else if (academic_grade >= 60 && academic_grade < 70)
+// {
+//     console.log("Your grade is D")
+// }
+
+// else if (academic_grade >= 70 && academic_grade < 80)
+// {
+//     console.log ("Your grade is C")
+// }
+// else if(academic_grade >= 80){
+//     console.log ("Your grade is B")
+
+// }
+// else {
+
+// }
